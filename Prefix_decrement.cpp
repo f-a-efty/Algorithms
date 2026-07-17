64445a95ef1a0305c012719f379c3d6bd8de1cda
Faria Ahmed Efty <eftyfariaahmed@gmail.com>
2026-07-17 18:35:24 +0600

    Prefix_decrement.cpp

--- Prefix_decrement.cpp
+++ Prefix_decrement.cpp
@@ -1,1 +1,28 @@
+//Prefix Decrement
 
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cin>>a;
+
+  while(--a){
+    cout<<"This line will print "<<a<<" times\n";
+  }
+}
+
+
+//Result
+/*
+6
+This line will print 5 times
+This line will print 4 times
+This line will print 3 times
+This line will print 2 times
+This line will print 1 times
+
+Analysis:
+In pre-decrement, first loop decreases a's value becomes 5 then it checks 5 is true or not.
+As it is true, then loop runs and prints "This line will print 5 times". 
+*/
